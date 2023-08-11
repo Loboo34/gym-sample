39ce922a697aa726f9405a4581c9f3b1f780ebed
loboo34 <rayovers40@gmail.com>
2023-08-11 07:50:25 +0300

    nav

--- index.js
+++ index.js
@@ -37,3 +37,11 @@ function reveal() {
 
 window.addEventListener("scroll", reveal);
 reveal();
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
